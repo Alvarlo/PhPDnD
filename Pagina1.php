2c456fe605254b87446012d28b3a37e34369fa9e
Alvarlo <alvaro.s401816@cesurformacion.com>
2025-03-03 14:19:32 +0100

    Pagina2

--- Pagina1.php
+++ Pagina1.php
@@ -9,31 +9,115 @@ if (isset($_REQUEST['nombre'])) {
 ?>
 
 <form action="Pagina2.php" method="post">
+
+    <h4>¿Dónde te gustaría vivir?</h4>
     <p>
-        <input type="checkbox" id="respuestaCheck1" name="vehicle1" value="Bike">
-        <label for="vehicle1"> I have a bike</label><br>
-        <input type="checkbox" id="respuestaCheck2" name="vehicle2" value="Car">
-        <label for="vehicle2"> I have a car</label><br>
-        <input type="checkbox" id="respuestaCheck3" name="vehicle3" value="Boat">
-        <label for="vehicle3"> I have a boat</label><br>
-    </p>
-    <p>
-        <input type="radio" id="html" name="fav_language" value="1">
-        <label for="1">Opcion1</label><br>
-        <input type="radio" id="html" name="fav_language" value="2">
-        <label for="2">Opcion2</label><br>
-        <input type="radio" id="html" name="fav_language" value="3">
-        <label for="3">Opcion3
-        </label><br>
+        <input type="radio" id="respuestaRadio1" name="lugar" value="bosque">
+        <label for="1"> Bosques y naturaleza</label><br>
+        <input type="radio" id="respuestaRadio2" name="lugar" value="montaña">
+        <label for="2">Montañas y lugares rocosos</label><br>
+        <input type="radio" id="respuestaRadio3" name="lugar" value="ciudad">
+        <label for="3">Ciudades y civilización</label><br>
     </p>
 
-
+    <h4>¿Qué cualidad te define mejor?</h4>
     <p>
         <select name="select">
-            <option value="value1">Value 1</option>
-            <option value="value2">Value 2</option>
-            <option value="value3">Value 3</option>
+            <option value="fuerzaYresistencia">Fuerza y resistencia</option>
+            <option value="inteligenciaYastucia">Inteligencia y astucia</option>
+            <option value="carismaYsociabilidad">Carisma y sociabilidad</option>
         </select>
     </p>
+    <h4>¿Qué habilidades te interesan?</h4>
+    <p>
+        <input type="checkbox" id="respuestaCheck1" name="habilidades" value="magia">
+        <label for="pregunta1">Magia y hechicería</label><br>
+        <input type="checkbox" id="respuestaCheck2" name="habilidades" value="combate">
+        <label for="pregunta2">Pelea y combate físico</label><br>
+        <input type="checkbox" id="respuestaCheck3" name="habilidades" value="sigilo">
+        <label for="pregunta3">Sigilo y destreza</label><br>
+    </p>
+</form>
 
+<?php
+/*
+Guardando las respuestas en session y se generará una raza para el personaje que
+también se guardará en session. El formulario debe redireccionar a pagina2.php.
+*/
+$enano = $elfo = $mediano = $humano = $draconido = $gnomo = $semielfo = $semiorco = $tiflin = 0;
+
+if (isset($_REQUEST['lugar'])) {
+    if ($_REQUEST['lugar'] === 'bosque') {
+        $elfo += 2;
+        $gnomo += 2;
+        $mediano += 1;
+    } elseif ($_REQUEST['lugar'] === 'montaña') {
+        $enano += 2;
+        $semiorco += 2;
+        $draconido += 1;
+    } elseif ($_REQUEST['lugar'] === 'ciudad') {
+        $humano += 2;
+        $semielfo += 2;
+        $tiflin += 1;
+    }
+}
+
+if (isset($_REQUEST['select'])) {
+    if ($_REQUEST['select'] === 'fuerzaYresistencia') {
+        $semiorco += 2;
+        $enano += 1;
+        $draconido += 1;
+    } elseif ($_REQUEST['select'] === 'inteligenciaYastucia') {
+        $gnomo += 2;
+        $tiflin += 1;
+        $elfo += 1;
+    } elseif ($_REQUEST['select'] === 'carismaYsociabilidad') {
+        $semielfo += 2;
+        $humano += 1;
+        $tiflin += 1;
+    }
+}
+
+if (isset($_REQUEST['habilidades'])) {
+    foreach ($_REQUEST['habilidades'] as $habilidad) {
+        if ($habilidad === 'magia') {
+            $tiflin += 1;
+            $elfo += 1;
+            $gnomo += 1;
+        } elseif ($habilidad === 'combate') {
+            $semiorco += 1;
+            $draconido += 1;
+            $enano += 1;
+        } elseif ($habilidad === 'sigilo') {
+            $mediano += 1;
+            $semielfo += 1;
+            $elfo += 1;
+        }
+    }
+}
+
+$razas = [
+    "Enano" => $enano,
+    "Elfo" => $elfo,
+    "Mediano" => $mediano,
+    "Humano" => $humano,
+    "Dracónido" => $draconido,
+    "Gnomo" => $gnomo,
+    "Semielfo" => $semielfo,
+    "Semiorco" => $semiorco,
+    "Tiflin" => $tiflin
+];
+$razaGanadora = array_search(max($razas), $razas);
+if($razaGanadora != 0){
+    setcookie('razaC', $razaGanadora);
+}else{
+    echo "No se ha guardado nada";
+}
+
+
+?>
+
+
+<form action="Pagina2.php" method="post">
+    <p><input type="submit" value="Enviar"></p>
 </form>
---
+++ Pagina2.php
@@ -0,0 +1,17 @@
+<!-- Se trata de una página php que contiene un texto con las distintas clases que hay en
+D&D y armas que usa cada clase. De igual forma que en pagina1.php se mostrará
+ahora el nombre del personaje y su raza para las preguntas que ayudarán a determinar
+la raza. Por ejemplo: “Aragorn el humano, ¿te gusta seducir dragones?”.
+
+Deberá haber al menos 4 preguntas que contendrán al menos una respuesta con
+number, otra con color, otra con un slider y otra con una fecha. El formulario no debe
+dejar que se envíe vacía ninguna de las respuestas.
+
+Guardando las respuestas en session y se generará una clase y un arma principal para
+el personaje que también se guardará en session. El formulario debe redireccionar a
+pagina3.php.
+ -->
+
+ <?php
+
+ ?>
